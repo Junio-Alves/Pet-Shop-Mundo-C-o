0480cb68c03cb5d110cab9b232fe539816f1f7b1
Ryan-auchi <ryan69407@gmail.com>
2025-02-08 04:21:50 -0300

    Update ryan madruga petshop.c
    basicamente so fiz foi mais bater cabeça pra ver oq precisa mais.
    
    mudei o listar_animais_entrada para listar qualquer lista, coloquei todas as funções pra baixo do main para nao ter problemas de usar funcoes que ainda nao tinham sido iniciadas, fiz o update e dei umas mexida no codigo pra tirar algumas coisas redundantes 
    
    como um while(sair != false) que nao precisava e outras partes pequenas que era so parte que nunca ia ser executada 
    
    a opcao Ver Animais da lista de espera acho que agora tambem ficou redundante ja que modifiquei o listar_animais_entrada

--- petshop.c
+++ petshop.c
@@ -1,7 +1,7 @@
 /*
-    Implemente um código em C com estruturas de dados e uso dinâmico da memória que seja capaz de manter 
-    o controle de entrada e saída de animais (gato ou cachorro), em um pet shop. 
-    O código deve representar os animais através de uma struct com os seguintes membros: nome, tutor, serviço e status. 
+    Implemente um código em C com estruturas de dados e uso dinâmico da memória que seja capaz de manter
+    o controle de entrada e saída de animais (gato ou cachorro), em um pet shop.
+    O código deve representar os animais através de uma struct com os seguintes membros: nome, tutor, serviço e status.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +11,7 @@
 // Definição da estrutura do nó
 // nome, tutor, serviço e status.
 typedef struct No {
-    int id; // ID 
+    int id; // ID
     char nome_animal[50];      // Nome do Animal
     char nome_tutor[50];  // Nome do tutor
     char servico[50]; // [0] banho, [1] tosa ou [2] ambos
@@ -22,12 +22,108 @@ typedef struct No {
 typedef struct Fila {
     No *inicio; // Ponteiro para o inicio da fila
     No *fim; // Ponteiro para o final
+    int quamt;
 } Fila;
 
 //PUBLICOS
-Fila fila; 
+Fila fila;
 int id_contador;
 
+Fila Andamento;
+Fila Finalizados;
+
+void inicializar_fila();
+
+//Para limpar o terminal, facilitando a leitura (falta ajustes)
+void limpa_terminal();
+
+//Verifica se a Fila está vazia
+bool isEmpty(No* no);
+
+//criar nó
+No* criar_no();
+
+//inserir na fila
+void inserir_fila(int id,char *nome_animal,char *nome_tutor,char *servico);
+
+//Imprime todos os animais na lista de espera
+void listar_animais(int opcao);
+
+
+//Função para liberar fila
+void liberar_fila(No *fila);
+
+// Função para capturar dados do usuário
+void entrada_dados(char *buffer, size_t tamanho) ;
+
+int escolher_servico(char *servico);
+
+void cadastrar_novo_animal();
+
+void modificar_cadastro();
+
+void mover_de_lista();
+
+
+int main() {
+    inicializar_fila();
+    id_contador = 0;
+    while (1) {
+        limpa_terminal();
+        int opcao;
+
+        // Exibe o menu de opções
+        printf("----------------------------------------------------------\n");
+        printf("Escolha uma opcao:\n");
+        printf("[1] - Cadastrar Novo Animal\n");
+        printf("[2] - Ver Animais da lista de espera\n");
+        printf("[3] - Remover Animal da lista de espera\n");
+        printf("[4] - Modificar Animal da lista de espera\n");
+        printf("[5] - Iniciar Serviço\n");
+        printf("[6] - Finalizar Serviço\n");
+        printf("[7] - Listar Animais em serviço\n"); //Ryan, liste as seguintes opções 0 - espera / 1 - finalizados / 2 - cancelados
+        printf("[8] - Entregar pets\n");
+        printf("[0] - Sair\n");
+        printf("Opcao: ");
+        printf("----------------------------------------------------------\n");
+        scanf("%i", &opcao);
+        getchar();
+
+        // Processa a escolha do usuário
+        switch (opcao) {
+            case 1:
+                cadastrar_novo_animal();
+                break;
+            case 2:
+                printf("%i",id_contador);
+                listar_animais(1);
+                break;
+            case 3:
+                break;
+            case 4:
+                modificar_cadastro();
+                break;
+            case 5:
+                break;
+            case 7:
+                printf("qual lista deseja listar?\n1-lista de espera\n2-lista de andamento\n3-lista de finalizados");
+                int resposta;
+                scanf("%d",&resposta);
+                getchar();
+                listar_animais(resposta);
+                break;
+            case 8:
+                break;
+            case 0:
+                // Sai do programa liberando a memória
+                printf("Saindo....");
+                return 0;
+            default:
+                // Opção inválida
+                printf("Opcao invalida\n");
+        }
+    }
+}
 void inicializar_fila() {
     fila.inicio = NULL;
     fila.fim = NULL;
@@ -62,7 +158,7 @@ No* criar_no(){
 
 //inserir na fila
 void inserir_fila(int id,char *nome_animal,char *nome_tutor,char *servico){
-    No* novo_no = criar_no(); 
+    No* novo_no = criar_no();
     novo_no->id = id;
     strcpy(novo_no->nome_animal, nome_animal);
     strcpy(novo_no->nome_tutor, nome_tutor);
@@ -79,9 +175,19 @@ void inserir_fila(int id,char *nome_animal,char *nome_tutor,char *servico){
 }
 
 //Imprime todos os animais na lista de espera
-void listar_animais_espera(){
+void listar_animais(int opcao){
+    No *atual;
+    if(opcao == 1) {
+        atual = fila.inicio;
+    }else if(opcao == 2) {
+        atual = Andamento.inicio;
+    }else if(opcao == 3) {
+        atual = Finalizados.inicio;
+    }else {
+        printf("opcao invalida. voltando pro menu.\n");
+        return;
+    }
     limpa_terminal();
-    No *atual = fila.inicio;
     if(isEmpty(atual)){
          printf("Fila Vazia\n");
          return;
@@ -115,8 +221,7 @@ void entrada_dados(char *buffer, size_t tamanho) {
 }
 
 int escolher_servico(char *servico){
-    bool sair = false;
-     while (sair != true) {
+     while (1) {
         limpa_terminal();
         int opcao;
 
@@ -136,21 +241,17 @@ int escolher_servico(char *servico){
         switch (opcao) {
             case 0:
                 strcpy(servico, "Banho");
-                sair = true;
                 return 0;
                 break;
             case 1:
                 strcpy(servico, "Tosa");
-                sair = true;
                 return 0;
                 break;
             case 2:
                 strcpy(servico, "Ambos");
-                sair = true;
                 return 0;
                 break;
             case 3:
-                sair = true;
                 return 1;
                 break;
             default:
@@ -176,59 +277,35 @@ void cadastrar_novo_animal(){
     };
     return;
 }
-
-
-
-int main() {
-    inicializar_fila();
-    id_contador = 0;
-    while (1) {
-        limpa_terminal();
-        int opcao;
-
-        // Exibe o menu de opções
-        printf("----------------------------------------------------------\n");
-        printf("Escolha uma opcao:\n");
-        printf("[1] - Cadastrar Novo Animal\n");
-        printf("[2] - Ver Animais da lista de espera\n");
-        printf("[3] - Remover Animal da lista de espera\n");
-        printf("[4] - Modificar Animal da lista de espera\n");
-        printf("[5] - Iniciar Serviço\n");
-        printf("[6] - Finalizar Serviço\n");
-        printf("[7] - Listar Animais em serviço\n"); //Ryan, liste as seguintes opções 0 - espera / 1 - finalizados / 2 - cancelados
-        printf("[8] - Entregar pets\n");
-        printf("[0] - Sair\n");
-        printf("Opcao: ");
-        printf("----------------------------------------------------------\n");
-        scanf("%d", &opcao);
-        getchar();
-
-        // Processa a escolha do usuário
-        switch (opcao) {
-            case 1:
-                cadastrar_novo_animal();
-                break;
-            case 2:
-                listar_animais_espera();
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 0:
-                // Sai do programa liberando a memória
-                printf("Saindo....");
-                return 0;
-            default:
-                // Opção inválida
-                printf("Opcao invalida\n");
+void modificar_cadastro() {
+    printf("qual cadastro deseja moficar?\n");
+    listar_animais(1);
+    printf("selecione o id:\n");
+    int resposta;
+    scanf("%d",&resposta);
+    getchar();
+    No *aux = fila.inicio;
+    for (int i = 0; i < 3; i++){
+        if(aux->id != resposta) {
+            aux = aux->proximo;
+        }else {
+            char nome_animal[50];
+            char nome_tutor[50];
+            char servico[50];
+            printf("Digite o nome do tutor: ");
+            entrada_dados(nome_tutor,sizeof(nome_tutor));
+            printf("Digite o nome do animal: ");
+            entrada_dados(nome_animal,sizeof(nome_animal));
+            if(escolher_servico(servico) != 1){
+                strcpy(aux->nome_animal, nome_animal);
+                strcpy(aux->nome_tutor, nome_tutor);
+                strcpy(aux->servico,servico);
+            }
+            return;
         }
     }
-    return 0;
+    printf("id nao encotrado\n");
+}
+void mover_de_lista() {
+
 }
